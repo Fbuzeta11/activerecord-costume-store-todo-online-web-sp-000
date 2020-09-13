7a955a753669a1b2380d92e30f7c6e821582f9ac
Federico Buzeta Borquez <fbuzeta11@gmail.com>
2020-09-13 02:34:20 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -4,6 +4,8 @@ class CreateCostumeStores < ActiveRecord::Migration[5.2]
     create_table :costume_stores do |t|
       t.string :name 
       t.string :location
+      t.integer :costume_inventory
+      t.integer :
   
   
   # Create your costume_stores migration here
