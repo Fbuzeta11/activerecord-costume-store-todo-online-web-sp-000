0f5751dcd0c6a7031311a4fb55e0b8cb1b6efdac
Federico Buzeta Borquez <fbuzeta11@gmail.com>
2020-09-13 02:41:05 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -9,6 +9,7 @@ class CreateHauntedHouses < ActiveRecord::Migration[5.2]
       t.boolean :family_friendly
       t.datetime :opening_time
       t.datetime :closing_time
+      t.text :description
     end
   end
 end# Create your haunted_houses migration here
